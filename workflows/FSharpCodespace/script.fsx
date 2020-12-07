4edb77391162a4dff3fc807a62e9f98ebf1abbfd
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2020-12-07 12:29:37 +0000

    example script with plot

--- workflows/FSharpCodespace/script.fsx
+++ workflows/FSharpCodespace/script.fsx
@@ -1,6 +1,14 @@
 #load "/workspaces/FSharpCodespace/dependencies/.paket/load/netstandard2.0/main.group.fsx"
 
+open System.IO
 open Plotly.NET
 
+
+Directory.GetDirectories("/arc/")
+
+Directory.GetFiles("/arc/assays/yourAssay/dataset")
+
+
+
 Chart.Point([1.,2.])
-|> Chart.SaveHtmlAs "test"
+|> Chart.SaveHtmlAs "/arc/runs/yourRunResultName/plot1"
